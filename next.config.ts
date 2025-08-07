c4bab540d78e920023cd9b99a626b912d7141b85
JakgritB <jakgrit.b@kkumail.com>
2025-08-07 17:48:55 +0700

    no ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 ปิด ESLint เวลา build
+  },
+
+  // ถ้ามี config อื่นอยู่แล้ว ให้เอามาไว้ด้วย เช่น:
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
